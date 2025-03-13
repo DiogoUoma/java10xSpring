982c9ea4390c44a1fdf985cfeda13e04ad4baf93
DiogoUoma <diogouoma3105@gmail.com>
2025-03-13 09:27:21 -0300

    refactor: inclui a annotation ToString nos modelsde Ninja

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
+public class NinjaControllerUI {
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUI(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<NinjaDTO>> listarNinjas() {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        return ResponseEntity.ok(ninjas);
+    }
+}
